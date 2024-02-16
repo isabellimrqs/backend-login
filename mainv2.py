6de79602d83bae829fa05c7233aca34688080087
Isabelli Maciel Marques <44590917807@>
2024-02-16 11:29:30 -0300

    criptografia

--- mainv2.py
+++ mainv2.py
@@ -2,6 +2,7 @@ from http.server import SimpleHTTPRequestHandler
 import socketserver
 import os 
 from urllib.parse import parse_qs, urlparse
+import hashlib
 
 class MyHandler(SimpleHTTPRequestHandler):
     def list_directory(self, path):
@@ -44,10 +45,10 @@ class MyHandler(SimpleHTTPRequestHandler):
 
         elif self.path.startswith('/cadastro'):
             query_pramas = parse_qs(urlparse(self.path).query)
-            login = query_pramas.get('login',[''])[0]
+            login = query_pramas.get('email',[''])[0]
             senha = query_pramas.get('senha',[''])[0]
 
-            welcome_message = f'Olá {login}, seja bem-vindo" Percebemos que você é novo por aqui'
+            welcome_message = f'Olá {login}, seja bem-vindo Percebemos que você é novo por aqui'
 
             self.send_response(200)
             self.send_header('Content-type', 'text/html;charset=utf-8')
@@ -56,7 +57,7 @@ class MyHandler(SimpleHTTPRequestHandler):
             with open(os.path.join(os.getcwd(), 'cadastro.html'),'r', encoding='utf-8') as cadastro_file:
                 content = cadastro_file.read()
 
-            content = content.replace('{login}',login)
+            content = content.replace('{email}',login)
             content = content.replace('{senha}',senha)
             content = content.replace('{welcome_message}',welcome_message)
 
@@ -71,13 +72,23 @@ class MyHandler(SimpleHTTPRequestHandler):
         with open('dados_login.txt','r') as file:
             for line in file:
                 if line.strip():
-                    stored_login, stored_senha,stored_nome = line.strip().split(';')
+                    stored_login, stored_senha_hash, stored_nome = line.strip().split(';')
                     if login == stored_login:
                         print('cheguei aqui significando que localizei o login informado')
-                        print('senha' + senha)
-                        print('senha armazenada' + senha)
-                        return senha == stored_senha
+                        print('senha ' + senha)
+                        print('senha armazenada ' + senha)
+
+                        senha_hash = hashlib.sha256(senha.encode('utf-8')).hexdigest()
+                        print(senha_hash)
+                        print(stored_senha_hash)
+                        return senha_hash == stored_senha_hash
             return False
+        
+    def adicionar_usuario(self,login,senha,nome):
+        senha_hash = hashlib.sha256(senha.encode('utf-8')).hexdigest()
+        print(senha_hash + 'senha hash')
+        with open('dados_login.txt', 'a', encoding='utf-8') as file:
+            file.write(f'{login};{senha_hash};{nome}\n')
         
     def remover_ultima_linha(self,arquivo):
         print('vou excluir a ultima linha')
@@ -101,33 +112,31 @@ class MyHandler(SimpleHTTPRequestHandler):
             senha = form_data.get('senha',[''])[0]
 
             if self.usuario_existente(login,senha):
-                 with open(os.path.join(os.getcwd(),'dados_existe.html'),'r',encoding='utf-8')as login_file:
-                    content = login_file.read()
                     self.send_response(200)
                     self.send_header('Content-type','text/html')
                     self.end_headers()
-                    self.wfile.write(content.encode('utf-8'))
+                    mensagem = f'Usuário {login} logado com sucesso!'
+                    self.wfile.write(mensagem.encode('utf-8'))
 
             else: 
                 if any(line.startswith(f'{login};')for line in open('dados_login.txt','r',encoding='utf-8')):
                             self.send_response(302)
                             self.send_header('Location','/login_failed')
                             self.end_headers()
-                            self.wfile.write(content.encode('utf-8'))
+
                             return
 
                 else:
-                    with open('dados_login.txt', 'a') as file:
-                            login = form_data.get('email',[''])[0]
-                            senha = form_data.get('senha',[''])[0]
-                            file.write(f"{login};{senha}\n")
 
-                    with open(os.path.join(os.getcwd(),'dados_ok.html'),'r',encoding='utf-8')as login_file:
-                        content = login_file.read()
-                    self.send_response(200)
-                    self.send_header('Content-type','text/html;charset=utf-8')
+
+                    self.adicionar_usuario(login,senha,nome='None')
+                    
+                    self.send_response(302)
+                    self.send_header('Location', f'/cadastro?login={login}&senha={senha}')
                     self.end_headers()
-                    self.wfile.write(content.encode('utf-8'))
+
+                    return
+                   
 
         elif self.path.startswith('/confirmar_cadastro'):
             content_lenght = int(self.headers['Content-Length'])
@@ -138,7 +147,7 @@ class MyHandler(SimpleHTTPRequestHandler):
             senha = form_data.get('senha',[''])[0]
             nome = form_data.get('nome',[''])[0]
 
-            print('nome' + nome)
+            senha_hash = hashlib.sha256(senha.encode('utf-8')).hexdigest()
 
             if self.usuario_existente(login, senha):
                 with open('dados_login.txt', 'r', encoding='utf-8') as file:
@@ -146,9 +155,9 @@ class MyHandler(SimpleHTTPRequestHandler):
 
                 with open('dados_login.txt', 'w', encoding='utf-8') as file:
                     for line in lines: 
-                        stored_login, stored_senha, stored_name = line.strip().split(';')
-                        if login == stored_login and senha == stored_senha:
-                            line = f'{login};{senha};{nome}\n'
+                        stored_login, stored_senha, stored_nome = line.strip().split(';')
+                        if login == stored_login and senha_hash == stored_senha:
+                            line = f'{login};{senha_hash};{nome}\n'
                         file.write(line)
 
                 self.send_response(302)
